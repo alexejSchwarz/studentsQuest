e1b9cbea25939a295dcb399dfc0e908ef67fff34
add054 <alexej.schwarz@haw-hamburg.de>
2025-04-05 22:33:34 +0200

    Coin Spawn Logik eingebaut (Spawnverhalten und Inkrementierung beim Einsammeln)

--- core/src/main/java/de/haw/sea2/gameLevel/SpawnLogic.java
+++ core/src/main/java/de/haw/sea2/gameLevel/SpawnLogic.java
@@ -1,9 +1,14 @@
 package de.haw.sea2.gameLevel;
 
-import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.utils.ImmutableArray;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
 import de.haw.sea2.StudentsQuest;
+import de.haw.sea2.ecs.ECSEngine;
 import de.haw.sea2.ecs.builders.EntityCreator;
+import de.haw.sea2.ecs.components.PlayerComponent;
 import de.haw.sea2.map.EntitySpawnPoint;
 import de.haw.sea2.map.mapObjectEnums.MapEntityTypes;
 
@@ -14,40 +19,42 @@ import de.haw.sea2.map.mapObjectEnums.MapEntityTypes;
 public class SpawnLogic {
 
     private final Array<EntitySpawnPoint> spawnPoints;
-    private final Array<EntitySpawnPoint> itemSpawnPoints;
+    private final Array<EntitySpawnPoint> availableItemSpawns;
     private final Array<EntitySpawnPoint> enemySpawnPoints;
-
+    private final ImmutableArray<Entity> players;
     private final EntityCreator creator;
+
+    private int collectedCoins;
+    private EntitySpawnPoint lastUsedItemSpawn;
 
     public SpawnLogic(StudentsQuest context) {
         this.spawnPoints = new Array<>();
-        this.itemSpawnPoints = new Array<>();
+        this.availableItemSpawns = new Array<>();
         this.enemySpawnPoints = new Array<>();
         this.creator = context.getEntityCreator();
+        this.players = context.getEngine().getEntitiesFor(Family.all(PlayerComponent.class).get());
+        this.collectedCoins = 0;
     }
 
     /**
      * Gruppierung der SpawnPoints und initiales Spawnen von Level / Map abhaengigen Entitäten
      */
-    public void prepareLevelStart(Array<EntitySpawnPoint> spawnPoints, Array<Rectangle> walls) {
+    public void prepareLevelStart(Array<EntitySpawnPoint> spawnPoints) {
         clear();
         this.spawnPoints.addAll(spawnPoints);
         for (EntitySpawnPoint spawnPoint : this.spawnPoints) {
             if (spawnPoint.entityType().equals(MapEntityTypes.ITEM.value)) {
-                this.itemSpawnPoints.add(spawnPoint);
+                this.availableItemSpawns.add(spawnPoint);
             } else if (spawnPoint.entityType().equals(MapEntityTypes.ENEMY.value)) {
                 this.enemySpawnPoints.add(spawnPoint);
             }
         }
 
+        spawnItem();
+
         // Spawn enemies
         for (EntitySpawnPoint spawnPoint : this.enemySpawnPoints) {
             spawnEnemy(spawnPoint);
-        }
-
-        // Existing logic for items
-        for (EntitySpawnPoint spawnPoint : this.itemSpawnPoints) {
-            spawnItem(spawnPoint);
         }
     }
 
@@ -60,17 +67,35 @@ public class SpawnLogic {
         this.creator.createEnemy(spawnPoint.spawnPoint(), 1f, 1f);
     }
 
-    private void spawnItem(EntitySpawnPoint spawnPoint) {
-        this.creator.createCoin(spawnPoint.spawnPoint(), 1f);
+    /**
+     * Spawnt neues Item. Es wird ein zufaelliger SpawnPunkt aus den moeglichen Punkten gewaehlt. Es wird verhindert, dass beim naechsten Methodenaufruf der gleiche Punkt
+     * genommen wird
+     */
+    private void spawnItem() {
+        int randomIndex = MathUtils.random(this.availableItemSpawns.size - 1);
+        EntitySpawnPoint newUsedItemSpawn = this.availableItemSpawns.removeIndex(randomIndex);
+        this.creator.createCoin(newUsedItemSpawn.spawnPoint(), 1f);
+
+        if (this.lastUsedItemSpawn != null) {
+            this.availableItemSpawns.add(this.lastUsedItemSpawn);
+        }
+        this.lastUsedItemSpawn = newUsedItemSpawn;
     }
 
     private void clear() {
         this.spawnPoints.clear();
-        this.itemSpawnPoints.clear();
+        this.availableItemSpawns.clear();
         this.enemySpawnPoints.clear();
     }
 
+    /**
+     * Aktualisiert die Spawnlogic, Sollen neue Gegner, Items etc. gespawnt werden?
+     */
     public void update(float delta) {
-        // TODO Logik für Enemy Spawns mit timer, Neue Items spawnen, wenn diese eingesammelt wurden etc
+        int newCoinCount = ECSEngine.PLAYER_COMP_MAPPER.get(this.players.get(0)).collectedCoins;
+        if (this.collectedCoins < newCoinCount) {
+            this.collectedCoins = newCoinCount;
+            spawnItem();
+        }
     }
 }
