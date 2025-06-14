2b7169d9855a8247c2b1b21cef0b3d6766145f0e
Jonas Krohn <jonas.krohn@haw-hamburg.de>
2025-06-14 18:36:10 +0200

    neue Map eingebaut

---
+++ assets/maps/firstMap/tsx/Cafe.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Cafe" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="../png/Cafe.png" width="264" height="264"/>
+</tileset>
---
+++ assets/maps/firstMap/tsx/Geiles_HAW_Set_01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Geiles_HAW_Set_01" tilewidth="16" tileheight="16" tilecount="289" columns="17">
+ <image source="../png/Geiles_HAW_Set_01.png" width="272" height="272"/>
+</tileset>
